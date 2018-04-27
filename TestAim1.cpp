22e632a6f1a7cca2ea9a683c9a5352e5bf66f52a
Garrick Hutcheson <garrick.hutcheson@gmail.com>
2018-04-26 18:14:36 -0700

    some comments and whatnot

--- TestAim1.cpp
+++ TestAim1.cpp
@@ -1,108 +1,105 @@
-#include <iostream>
-#include "HashHelpers.h"
 #include "AimHelper.h"
+#include "HashHelpers.h"
 #include "LinearProbing.h"
 #include "QuadraticProbing.h"
 #include "SeparateChaining.h"
+#include <iostream>
 
 using namespace std;
 
 int main() {
+
   /*
-    opens input file Ohenry.txt
-    loads contents to vector of strings - dataArray
+    open input file Ohenry.txt
+    load contents to vector of strings - dataArray
   */
   vector<string> dataArray;
-  vector<string> queryArray;
-
-  string fileName = "OHenry.txt";//line not used
-
   fileToArray("OHenry.txt", &dataArray);
+  cout << "Data Array Size: " << dataArray.size() <<endl;
+  /*
+   open queries.txt
+   load contents to vector of strings "queryArray"
+ */
+  vector<string> queryArray;
   fileToArray("queries.txt", &queryArray);
 
-  ChainingHashTable<string> cHash;
-  QuadraticHashTable<string> qHash;
-  LinearHashTable<string> lHash;
-
-  cHash.insertArray(&dataArray);
-  qHash.insertArray(&dataArray);
-  lHash.insertArray(&dataArray);
-
-  //print some stuff
-  cout << "\nChaining "<<"\n";
-  cout << "ElapsedTime " << cHash.getElapsedTime() << "\n";
-  cout << "Collisions " << cHash.getCollisions() << "\n";
-   //<<"#UnsuccessfulProbes " << cHash.getUnsuccessfulProbes() << "\n"
-  //cout << "We found " << searchByQueryC(&queryArray, &cHash)<< "out of" << queryArray.size() << "\n"<<endl;
-
-  cout << "\nQuadratic Probing "<<"\n";
-  cout << "ElapsedTime " << qHash.getElapsedTime() << "\n";
-  cout << "Collisions " << qHash.getCollisions() << "\n";
-  cout <<"#UnsuccessfulProbes " << qHash.getUnsuccessfulProbes() << "\n";
-  cout << "We found " << searchByQueryQ(&queryArray, &qHash)<< "/" << queryArray.size() << "\n"<<endl;
-
-  cout << "\nLinear Probing "<<"\n";
-  cout << "ElapsedTime " << lHash.getElapsedTime() << "\n";
-  cout << "Collisions " << lHash.getCollisions() << "\n";
-  cout <<"#UnsuccessfulProbes " << lHash.getUnsuccessfulProbes() << "\n";
-  //cout << "We found " << searchByQueryL(&queryArray, &lHash) <<"/" << queryArray.size() << "\n"<<endl;
-
-
-
   /*
-  for(vector<string>::iterator iter = dataArray.begin(); iter < dataArray.end(); iter++)
-  {
-    cout << *iter <<endl;
-  }
-  
-  
-  cout << "\n\n\n\n\n\n" << endl;
-  
-  getchar();
-  
-  for(vector<string>::iterator iter = queryArray.begin(); iter < queryArray.end(); iter++)
-  {
+  instantiate all three hashtables
+    ChainingHT, linearProbingHT, QuadraticProbingHT
+*/
+  ChainingHashTable<string> ChainingHT;
+  QuadraticHashTable<string> QuadraticProbingHT;
+  LinearHashTable<string> LinearProbingHT;
+
+  ChainingHT.insertArray(&dataArray);         // InsertIntoChainingHT
+  cout << "ChainingHT Current size:" << ChainingHT.getCurrentSize() << endl;
+  QuadraticProbingHT.insertArray(&dataArray); // InsertIntoQuadraticHT
+    cout << "QuadraticHT Current size:" << QuadraticProbingHT.getCurrentSize() << endl;
+  LinearProbingHT.insertArray(&dataArray);    // InsertIntoLinearHT
+  cout << "LinearProbingHT Current size:" << LinearProbingHT.getCurrentSize() << endl;
+
+  // print some stuff
+  cout << "\nChaining "
+       << "\n";
+  cout << "ElapsedTime " << ChainingHT.getElapsedTime() << "\n";
+  cout << "Collisions " << QuadraticProbingHT.getCollisions() << "\n";
+  //<<"#UnsuccessfulProbes " << ChainingHT.getUnsuccessfulProbes() << "\n"
+  cout << ChainingHT.contains("cop") << endl;
+  // searchByQueryC(&queryArray, &ChainingHT);
+  //<< "out of" << queryArray.size() << "\n"<<endl;
+
+  cout << "\nQuadratic Probing "
+       << "\n";
+  cout << "ElapsedTime " << QuadraticProbingHT.getElapsedTime() << "\n";
+  cout << "Collisions " << QuadraticProbingHT.getCollisions() << "\n";
+  cout << "#UnsuccessfulProbes " << QuadraticProbingHT.getUnsuccessfulProbes() << "\n";
+  // cout << "We found " << searchByQueryQ(&queryArray, &QuadraticProbingHT)<< "/" <<
+  // queryArray.size() << "\n"<<endl;
+
+  cout << "\nLinear Probing "
+       << "\n";
+  cout << "ElapsedTime " << LinearProbingHT.getElapsedTime() << "\n";
+  cout << "Collisions " << LinearProbingHT.getCollisions() << "\n";
+  cout << "#UnsuccessfulProbes " << LinearProbingHT.getUnsuccessfulProbes() << "\n";
+  // cout << "We found " << searchByQueryL(&queryArray, &LinearProbingHT) <<"/" <<
+  // queryArray.size() << "\n"<<endl;
+/*
+  for (vector<string>::iterator iter = dataArray.begin();
+       iter < dataArray.end(); iter++) {
     cout << *iter << endl;
   }
-  */
-  
-  /*
-    opens queries.txt
-    loads contents to vector of strings "queryArray"
-  */
-  
-  /*
-    instantiates all three hashtables
-      ChainingHT, linearProbingHT, QuadraticProbingHT
-  */
-  
+
+  cout << "\n\n\n\n\n\n" << endl;
+
+  getchar();
+
+  for (vector<string>::iterator iter = queryArray.begin();
+       iter < queryArray.end(); iter++) {
+    cout << *iter << endl;
+  }
+*/
   /*
     ANALYSIS OF CHAINING
     call function insertIntoChainingHT(DataArray)
     Initializes INSERTIONTIMERCHAININGHT
     initialises and keeps track of CollisionsChainingHT that counts total
-     number of collisions accross all insertions (Note  that  this  is  same  as  counting  the 
-    total  number  of  times  you  are  inside  the  while  loop  within 
-    function findPos() across all insertions.)
-    calls SearchChaingingHT(QueryArray)
+     number of collisions accross all insertions (Note  that  this  is  same  as
+    counting  the total  number  of  times  you  are  inside  the  while  loop
+    within function findPos() across all insertions.) calls
+    SearchChaingingHT(QueryArray)
   */
-  
-  
-  /*
-    insertIntoChainingHT(array)
-    inserts words into DataArray no duplicate words are allowed
-  */
-  
+
+
+
   /*
     insertionTimerChainingHT()
     private member holds the sum of times taken for all insertions
     function adds an insert time to private member
   */
-  
+
   /*
     SearchChaingingHT(QueryArray)
-    does a find for every query in the list of queries 
-    adds time 
+    does a find for every query in the list of queries
+    adds time
   */
-  
 }
